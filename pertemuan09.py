9fd43d2386736a6c6f762631f373c137f9a40704
Sherly S Rindiani <sherlysenja.trisakti@gmail.com>
2023-11-11 15:11:15 +0700

    Menambahkan metric

--- pertemuan09.py
+++ pertemuan09.py
@@ -1,19 +1,27 @@
 import streamlit as st
 
+# Ini bagian heading aplikasi streamlit
 st.title("Kuliah Praktikum Big Data")
 st.write("Sherly Senja")
 st.write("# Heading 1")
 st.write("## Heading 2")
 st.write("### Heading 3")
+st.write("#### Heading 4")
 
-st.metric(
+# Kinerja unit
+st.metric("Kinerja",40, -1)
+st.metric("Response Time",30, 20)
 
+#Pilihan checkbox
 pilih1 = st.checkbox('Ya')
 pilih2 = st.checkbox('Tidak')
 
 st.write(pilih1)
 st.write(pilih2)
 
+'''
+Ini komentar harusnya
+'''
 makanan = st.radio('Makanan kesukaan',['Bakso','Mie Ayam','Nasi Goreng','Mie Goreng'])
 st.write(makanan)
 
